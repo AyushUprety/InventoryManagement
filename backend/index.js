04dfd38e7d077a7c4c7a1f42bbf40246732d07e0
Ayush Uprety <ayushuprety1@gmail.com>
2023-04-26 10:23:28 +0545

    Added a homepage route in index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+// Setting up the routes
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
